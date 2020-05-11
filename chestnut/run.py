7e0f944ca6549cfab115375b7f1974066f95cc03
Mingwei Samuel <mingwei.samuel@gmail.com>
2020-05-11 12:04:23 -0700

    Update run.py to change sys.stdout before gurobi can print

--- chestnut/run.py
+++ chestnut/run.py
@@ -1,9 +1,9 @@
-import os, argparse
+import sys, os, argparse
 
 parser = argparse.ArgumentParser(description = 'Run Chestnut Scripts for Kandan.')
 parser.add_argument('--single_query', type = int, default = -1,
         help = 'Run ILP on a single query (omit for all)')
-parser.add_argument('--scale', type = float,
+parser.add_argument('--scale', type = float, default = 0, # TODO?
         help = 'Data scale for TSV (optional).')
 parser.add_argument('--membound_factor', type = float, default = 1.7,
         help = 'Membound factor, should be greater than 1. Default: 1.7')
@@ -19,9 +19,19 @@ args = parser.parse_args()
 if args.scale is not None:
     os.environ['CHESTNUT_SCALE'] = str(args.scale)
 
+
+# Setup quiet
+old_stdout = sys.stdout
+devnull = open(os.devnull, 'w')
+if not args.vvv:
+    sys.stdout = devnull
+
 # Load kandan and run.
 from repo.benchmark.kandan import kandan
 kandan.run(single_query = args.single_query,
     gen_tsv = True, gen_cpp = args.gen_cpp,
     membound_factor = args.membound_factor,
-    run_test_read_overall = False, quiet = not args.vvv)
+    run_test_read_overall = False, output = old_stdout)
+
+sys.stdout = old_stdout
+devnull.close()
